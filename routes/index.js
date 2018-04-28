026fb36a3c3734f0bcf76270f34f2b4deef24a7f
SweetRain16 <37534993+SweetRain16@users.noreply.github.com>
2018-04-28 11:39:29 +0900

    5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' contents: '테스트 첫번째 화면'});
+});
+
+router.get('/list', function(req, res, next) {
+  res.render('list', {
+    lists: [
+      '1',
+      '2',
+      '3',
+      '4'
+    ]
+  });
+});
+
+router.get('/hello', function(req, res, next) {
+  res.render('hello', { title: '안녕' contents: '테스트 두번쨰 화면'});
 });
 
 module.exports = router;
